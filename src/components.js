de13f71976af152d36d9f8d60a607ab9fce983c8
Anselem <anselemodims@gmail.com>
2021-12-20 18:33:29 +0100

    Feat: added styles for the header

--- src/components.js
+++ src/components.js
@@ -4,16 +4,15 @@ const Nav = () => `
   <nav>
     <div class="logo">
         <img src=${Logo} alt="site logo" />
-        <p>Awesome Books</p>
       </div>
       <ul>
         <li>
-          <a href="#" class="links active" data-content="#books">Lists</a>
+          <a href="#" class="links active" data-content="#books">TV Shows</a>
         </li>
         <li>
-          <a href="#" class="links" data-content="#add-book">Add New</a>
+          <a href="#" class="links" data-content="#add-book">Series</a>
         </li>
-        <li><a href="#" class="links" data-content="#contact">Contact</a></li>
+        <li><a href="#" class="links" data-content="#contact">Movies</a></li>
       </ul>
   </nav>
   `;

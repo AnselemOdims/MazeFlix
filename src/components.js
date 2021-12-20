51439a0defc7021dfb7df1dabfee1e8d41469329
Anselem <anselemodims@gmail.com>
2021-12-20 17:50:06 +0100

    Feat: included the site logo

--- src/components.js
+++ src/components.js
@@ -1,19 +1,17 @@
-const Header = () => {
-  return `
+const Header = () => `
   <nav>
-      <div class="logo">
-          <img src="book-reader-solid.svg" alt="site logo" />
-          <p>Awesome Books</p>
-        </div>
-        <ul>
-          <li>
-            <a href="#" class="links active" data-content="#books">Lists</a>
-          </li>
-          <li>
-            <a href="#" class="links" data-content="#add-book">Add New</a>
-          </li>
-          <li><a href="#" class="links" data-content="#contact">Contact</a></li>
-        </ul>
-      </nav>
-  `
-}
+    <div class="logo">
+        <img src="book-reader-solid.svg" alt="site logo" />
+        <p>Awesome Books</p>
+      </div>
+      <ul>
+        <li>
+          <a href="#" class="links active" data-content="#books">Lists</a>
+        </li>
+        <li>
+          <a href="#" class="links" data-content="#add-book">Add New</a>
+        </li>
+        <li><a href="#" class="links" data-content="#contact">Contact</a></li>
+      </ul>
+  </nav>
+  `;

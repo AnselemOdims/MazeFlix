a7feabb9236d39465462b8929f0f91464abee583
Anselem <anselemodims@gmail.com>
2021-12-20 19:07:52 +0100

    Feat: added media query for the header

--- src/components.js
+++ src/components.js
@@ -1,13 +1,13 @@
 import Logo from './assets/images/mAZEFLIX.gif';
 
-const Nav = () => `
+const Nav = (num = 6) => `
   <nav>
     <div class="logo">
         <img src=${Logo} alt="site logo" />
       </div>
       <ul>
         <li>
-          <a href="#" class="links active" data-content="#books">TV Shows</a>
+          <a href="#" class="links active" data-content="#books">TV Shows(${num})</a>
         </li>
         <li>
           <a href="#" class="links" data-content="#add-book">Series</a>

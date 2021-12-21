b7f0c72ebc13c50ed45c031ca877d292dc16452d
Anselem <anselemodims@gmail.com>
2021-12-21 09:18:41 +0100

    Feat: added the render static method

--- src/utils.js
+++ src/utils.js
@@ -4,5 +4,14 @@ import FetchWrapper from './fetchWrapper.js';
  * @class Utils - the utility class to hold all utiltity functionality
  */
 export default class Utils {
-  #API = new FetchWrapper('https://us-central1-js-capstone-backend.cloudfunctions.net/api/');
+  #tvAPI = new FetchWrapper('https://api.tvmaze.com/search/shows?q=a');
+
+  /**
+   * @function render - handles the rendering of the server response on the DOM
+   * @param {Array} data - the array response from the server
+   */
+  static render({ data, list } = {}) {
+    document.querySelector('#lists-container').innerHTML = data.map((result) => list(result))
+      .join('');
+  }
 }

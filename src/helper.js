fb31a230457ca255e85cc4421babbf38be6b3abf
Anselem <anselemodims@gmail.com>
2021-12-21 09:45:36 +0100

    Feat: craeted the getHandler static method

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import Utils from './utils.js';
 import FetchWrapper from './fetchWrapper.js';
 
@@ -5,5 +6,14 @@ import FetchWrapper from './fetchWrapper.js';
  * @class Helper - the helper class
  */
 export default class Helper {
+  #tvAPI = new FetchWrapper('https://api.tvmaze.com/');
 
+  /**
+   * @function getHandler - handles the refresh button click event
+   * @param {Event Object} e - the event object
+   */
+  async getHandler({ input } = {}) {
+    const { result } = await this.#tvAPI.get(`search/shows?q=${input}`);
+    return result.json();
+  }
 }

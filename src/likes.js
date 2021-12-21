fe1aea3e5ee5779141eb86c75f7291b7e9ee2b7c
Anselem <anselemodims@gmail.com>
2021-12-21 13:29:52 +0100

    Feat: created the postLikes instance method

--- src/likes.js
+++ src/likes.js
@@ -1,6 +1,24 @@
+/* eslint-disable class-methods-use-this */
+import FetchWrapper from './fetchWrapper.js';
 /**
  * @class Likes - handles all likes related methods
  */
-class Likes {
+export default class Likes {
+  static #API = new FetchWrapper('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/');
 
+  static #appID = 'bPBKRZKblKYzssiYo25l';
+
+  /**
+   * @instance method
+   * @param {String} param0 - the id of the item
+   * @returns - the server response
+   * @memberof Likes
+   */
+  async postLikes({ id } = {}) {
+    const data = {
+      item_id: Number(id),
+    };
+    const response = await Likes.#API.post(`apps/${Likes.#appID}/likes`, data);
+    return response;
+  }
 }

55c964a07ebbc76bfe6fb48ea93ee0f5520a3a61
Anselem <anselemodims@gmail.com>
2021-12-21 09:19:49 +0100

    Feat: added the startLoader static method

--- src/utils.js
+++ src/utils.js
@@ -14,4 +14,14 @@ export default class Utils {
     document.querySelector('#lists-container').innerHTML = data.map((result) => list(result))
       .join('');
   }
+
+  /**
+ * @function startLoader - handles the start of the spinner loading
+ * @param {HTMLElement} elem1 - the first HTMLElement
+ * @param {HTMLElement} elem2 - the second HTMLElement
+ */
+  static startLoader(elem1, elem2) {
+    elem2.classList.add('loading-spinner');
+    elem1.innerHTML = 'Loading';
+  }
 }

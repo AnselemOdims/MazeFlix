c9faa59bcf8ac3ea7b98a31bc622809e16174887
Anselem <anselemodims@gmail.com>
2021-12-21 05:30:22 +0100

    Feat: added form markup

--- src/components.js
+++ src/components.js
@@ -3,17 +3,19 @@ import Logo from './assets/images/mAZEFLIX.gif';
 const Nav = (num = 6) => `
   <nav>
     <div class="logo">
-        <img src=${Logo} alt="site logo" />
-      </div>
-      <ul>
-        <li>
-          <a href="#" class="links active" data-content="#books">TV Shows(${num})</a>
-        </li>
-        <li>
-          <a href="#" class="links" data-content="#add-book">Series</a>
-        </li>
-        <li><a href="#" class="links" data-content="#contact">Movies</a></li>
-      </ul>
+      <img src=${Logo} alt="site logo" />
+    </div>
+    <div>
+      <form>
+        <input type="search" placeholder="search"/>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+    <ul>
+      <li>
+        <a href="#" class="links active" data-content="#books">TV Shows(${num})</a>
+      </li>
+    </ul>
   </nav>
   `;
 
